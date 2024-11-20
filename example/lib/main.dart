3da156997acc06bfd393efac9cb730a100509146
Israel Rodrigues <r.israel0210@gmail.com>
2024-11-20 03:43:26 -0300

    feat: add splash page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:example/src/controller/home_controller.dart';
+import 'package:example/src/local_db/cache.dart';
 import 'package:example/src/view/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'utils/theme/theme.dart';
 
-void main() {
+void main() async {
   //Initialized manually because 'GetMaterialApp' depends on this dependency injection.
-  Get.put(HomeController());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await GetStorage.init();
+  Get.put(HomeController(cache: Cache()));
   runApp(const MyApp());
 }
 
@@ -24,6 +30,9 @@ class MyApp extends GetView<HomeController> {
         themeMode: controller.themeMode,
         debugShowCheckedModeBanner: false,
         home: const HomePage(),
+        onInit: () {
+          FlutterNativeSplash.remove();
+        },
       );
     });
   }
