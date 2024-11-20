e7a3e7b8db067f2b29bd0ed21044375b679cbe04
Israel Rodrigues <r.israel0210@gmail.com>
2024-11-19 23:15:55 -0300

    refac: qr code generation discontinued

--- ecommerce_package/lib/src/views/product_detail_view.dart
+++ ecommerce_package/lib/src/views/product_detail_view.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ecommerce_package/src/controller/product_controller.dart';
 import 'package:ecommerce_package/src/models/product_model.dart';
-import 'package:ecommerce_package/src/views/widgets/qr_code_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:share_plus/share_plus.dart';
@@ -25,75 +24,17 @@ class ProductDetailView extends StatelessWidget {
       child: CircularProgressIndicator(color: theme.colorScheme.surface),
     );
 
-    Future<void> onSelectTypeShareProduct() async {
-      await Get.bottomSheet(
-        Container(
-          height: 100,
-          decoration: BoxDecoration(
-            color: theme.scaffoldBackgroundColor,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(16),
-              topRight: Radius.circular(16),
-            ),
-          ),
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Container(
-                width: 50,
-                height: 8,
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.outline.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextButton(
-                      onPressed: () {
-                        Get.back();
-                        Get.dialog(
-                            QRCodeWidget(product: product, theme: theme));
-                      },
-                      child: Text(
-                        'Gerar Qr Code',
-                        style: theme.textTheme.bodyMedium!.copyWith(
-                          color: theme.primaryColor,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: theme.colorScheme.surface,
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      onPressed: () async {
-                        Get.back();
-                        final message =
-                            'Confira este produto: ${product.name}.\n\nPreço: ${product.price.toStringAsFixed(2)}.\n\nimage: ${product.images[0]}';
-                        Get.dialog(loading);
-                        await Share.share(message);
-                        Get.back();
-                      },
-                      icon: const Icon(Icons.share),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(title: const Text('Detalhes')),
       floatingActionButton: FloatingActionButton(
         shape: const CircleBorder(),
-        onPressed: onSelectTypeShareProduct,
+        onPressed: () async {
+          final message =
+              'Confira este produto: ${product.name}.\n\nPreço: ${product.price.toStringAsFixed(2)}.\n\nimage: ${product.images[0]}';
+          Get.dialog(loading);
+          await Share.share(message);
+          Get.back();
+        },
         backgroundColor: theme.colorScheme.outline,
         child: Icon(
           Icons.share,
